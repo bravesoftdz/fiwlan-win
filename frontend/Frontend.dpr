e63dd2e33200e95674fa555c08411b62d7aee724
zawuza <andreizawuza@gmail.com>
2017-03-05 00:25:01 +0100

    Progressing web fronted

--- frontend/Frontend.dpr
+++ frontend/Frontend.dpr
@@ -3,7 +3,12 @@ program Frontend;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fMainView in 'fMainView.pas' {Form1};
+  fMainView in 'fMainView.pas' {Form1},
+  ceffmx in 'dep\dcef3\ceffmx.pas',
+  cefgui in 'dep\dcef3\cefgui.pas',
+  ceflib in 'dep\dcef3\ceflib.pas',
+  cefvcl in 'dep\dcef3\cefvcl.pas',
+  ceferr in 'dep\dcef3\ceferr.pas';
 
 {$R *.res}
 
--- frontend/fMainView.pas
+++ frontend/fMainView.pas
@@ -5,10 +5,12 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Sensors,
-  System.Sensors.Components, FMX.Controls.Presentation, FMX.StdCtrls;
+  System.Sensors.Components, FMX.Controls.Presentation, FMX.StdCtrls, ceffmx;
 
 type
   TForm1 = class(TForm)
+    ChromiumFMX1: TChromiumFMX;
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,5 +23,11 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  ChromiumFMX1.Load
+    ('file://C:/Prog/scb2017/fiwlan-win/frontend/Win32/Debug/maprender.html');
+end;
 
 end.
